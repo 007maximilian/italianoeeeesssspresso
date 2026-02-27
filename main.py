fdd410d64c4d93e99fc3cee71b7d8666f1f98769
007maximilian <max.mazu007@gmail.com>
2026-02-27 06:57:49 +0300

    capuchinno italianno

--- main.py
+++ main.py
@@ -1,13 +1,57 @@
 import sys
 import sqlite3
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem, QDialog, QMessageBox
+
+class AddEditCoffeeForm(QDialog):
+    def __init__(self, parent=None, coffee_id=None):
+        super().__init__(parent)
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.coffee_id = coffee_id
+        
+        if coffee_id:
+            self.setWindowTitle('Edit Coffee')
+            self.load_coffee_data()
+        else:
+            self.setWindowTitle('Add Coffee')
+    
+    def load_coffee_data(self):
+        connection = sqlite3.connect('coffee.sqlite')
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM coffee WHERE id = ?', (self.coffee_id,))
+        coffee = cursor.fetchone()
+        connection.close()
+        
+        if coffee:
+            self.nameEdit.setText(coffee[1])
+            index = self.roastCombo.findText(coffee[2])
+            self.roastCombo.setCurrentIndex(index)
+            index = self.typeCombo.findText(coffee[3])
+            self.typeCombo.setCurrentIndex(index)
+            self.tasteEdit.setText(coffee[4])
+            self.priceSpin.setValue(coffee[5])
+            self.volumeSpin.setValue(coffee[6])
+    
+    def get_data(self):
+        return (
+            self.nameEdit.text(),
+            self.roastCombo.currentText(),
+            self.typeCombo.currentText(),
+            self.tasteEdit.toPlainText(),
+            self.priceSpin.value(),
+            self.volumeSpin.value()
+        )
 
 class CoffeeWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
-        self.loadButton.clicked.connect(self.load_coffee_data)
+        
+        self.addButton.clicked.connect(self.add_coffee)
+        self.editButton.clicked.connect(self.edit_coffee)
+        self.deleteButton.clicked.connect(self.delete_coffee)
+        self.refreshButton.clicked.connect(self.load_coffee_data)
+        
         self.load_coffee_data()
     
     def load_coffee_data(self):
@@ -19,6 +63,7 @@ class CoffeeWindow(QMainWindow):
         
         self.tableWidget.setRowCount(len(data))
         self.tableWidget.setColumnCount(7)
+        self.tableWidget.setHorizontalHeaderLabels(['ID', 'Name', 'Roast', 'Type', 'Taste', 'Price', 'Volume'])
         
         for i, row in enumerate(data):
             for j, value in enumerate(row):
@@ -26,6 +71,65 @@ class CoffeeWindow(QMainWindow):
                 self.tableWidget.setItem(i, j, item)
         
         self.tableWidget.resizeColumnsToContents()
+    
+    def add_coffee(self):
+        dialog = AddEditCoffeeForm(self)
+        if dialog.exec_():
+            data = dialog.get_data()
+            connection = sqlite3.connect('coffee.sqlite')
+            cursor = connection.cursor()
+            cursor.execute('''
+                INSERT INTO coffee (name, roast_level, ground_or_beans, taste_description, price, package_volume)
+                VALUES (?, ?, ?, ?, ?, ?)
+            ''', data)
+            connection.commit()
+            connection.close()
+            self.load_coffee_data()
+            QMessageBox.information(self, 'Success', 'Coffee added successfully!')
+    
+    def edit_coffee(self):
+        current_row = self.tableWidget.currentRow()
+        if current_row < 0:
+            QMessageBox.warning(self, 'Warning', 'Please select a coffee to edit')
+            return
+        
+        coffee_id = int(self.tableWidget.item(current_row, 0).text())
+        dialog = AddEditCoffeeForm(self, coffee_id)
+        if dialog.exec_():
+            data = dialog.get_data()
+            connection = sqlite3.connect('coffee.sqlite')
+            cursor = connection.cursor()
+            cursor.execute('''
+                UPDATE coffee 
+                SET name=?, roast_level=?, ground_or_beans=?, taste_description=?, price=?, package_volume=?
+                WHERE id=?
+            ''', (*data, coffee_id))
+            connection.commit()
+            connection.close()
+            self.load_coffee_data()
+            QMessageBox.information(self, 'Success', 'Coffee updated successfully!')
+    
+    def delete_coffee(self):
+        current_row = self.tableWidget.currentRow()
+        if current_row < 0:
+            QMessageBox.warning(self, 'Warning', 'Please select a coffee to delete')
+            return
+        
+        coffee_id = int(self.tableWidget.item(current_row, 0).text())
+        coffee_name = self.tableWidget.item(current_row, 1).text()
+        
+        reply = QMessageBox.question(self, 'Confirm Delete', 
+                                    f'Are you sure you want to delete {coffee_name}?',
+                                    QMessageBox.Yes | QMessageBox.No)
+        
+        if reply == QMessageBox.Yes:
+            connection = sqlite3.connect('coffee.sqlite')
+            cursor = connection.cursor()
+            cursor.execute('DELETE FROM coffee WHERE id = ?', (coffee_id,))
+            connection.commit()
+            connection.close()
+            self.load_coffee_data()
+            QMessageBox.information(self, 'Success', 'Coffee deleted successfully!')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
